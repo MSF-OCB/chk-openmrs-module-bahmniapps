1387814e1dfa7487c97a58b2f1e6a8172816b225
sumanmaity112 <suman.maity112@gmail.com>
2017-11-22 15:08:17 +0530

    Suman | #000 | Enable i18n for privilege required message

--- ui/app/common/constants.js
+++ ui/app/common/constants.js
@@ -179,7 +179,7 @@ Bahmni.Common = Bahmni.Common || {};
         nutritionalConceptName: 'Nutritional Values',
         messageForNoObservation: "No observations captured for this visit.",
         messageForNoDisposition: "NO_DISPOSTIONS_AVAILABLE_MESSAGE_KEY",
-        messageForNoFulfillment: "No observations captured for this order.",
+        messageForNoFulfillment: "NO_FULFILMENT_MESSAGE",
         reportsUrl: "/bahmnireports",
         uploadReportTemplateUrl: "/bahmnireports/upload",
         ruledOutdiagnosisStatus: "Ruled Out Diagnosis",
@@ -251,7 +251,7 @@ Bahmni.Common = Bahmni.Common || {};
         conditionHistoryUrl: EMRAPI + '/conditionhistory',
         followUpConditionConcept: 'Follow-up Condition',
         localeLangs: "/bahmni_config/openmrs/apps/home/locale_languages.json",
-        privilegeRequiredErrorMessage: "User is logged in but doesn't have the relevant privilege"
+        privilegeRequiredErrorMessage: "PRIVILEGE_REQUIRED"
     };
 })();
 
